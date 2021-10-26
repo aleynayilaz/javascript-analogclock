dfe8452b9bcc4b16ce9298a2aec154add5396520
aleynayilaz <yilazaleyna.56@gmail.com>
2021-10-26 19:22:19 +0300

    number

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const mn= document.querySelector("#mn")
 const sc= document.querySelector("#sc")
 const number=document.querySelector("number")
 
+let degree=0;
+let clock=document.getElementsByClassName('clock')[0]
+for(let i = 1; i <= 12; i++) {
+    degree +=30;
+    let element = document.createElement("span")
+    element.className = 'number'
+    element.innerText = i;
+    element.style.transform = `rotate(${degree}deg)`
+    clock.appendChild(element) 
+}
+
 setInterval(() => {
     let day = new Date();
     let hh = day.getHours() * 30;
